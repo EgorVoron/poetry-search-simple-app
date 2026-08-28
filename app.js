4a184ed0f2ea788969671fdf4ab1921a821c2dcd
EgorVoron <ekvoron01@gmail.com>
2026-08-28 19:16:35 +0300

    fix url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-API_URL = "https://poetry-search.ru:8000"
+API_URL = "https://poetry-search.ru:app"
 
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
